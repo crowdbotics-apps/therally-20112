46974ac9bab6ac314a0866af88bd8bfbc7c37003
Crowdbotics <team@crowdbotics.com>
2020-09-08 18:28:10 +0000

    Initial commit.

---
+++ therally_20112/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for therally_20112 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'therally_20112.settings')
+
+application = get_wsgi_application()
